9639455deb6c2412a6ceef5fa9c9c4e7a791ab96
Nived <nived@globalsurf.ae>
2025-09-23 12:26:40 +0530

    feat:cron job github workflow added

---
+++ app/api/run/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import { DataAPIClient } from "@datastax/astra-db-ts";
+
+const client = new DataAPIClient(process.env.ASTRA_DB_APPLICATION_TOKEN!);
+const database = client.db(process.env.ASTRA_DB_API_ENDPOINT!);
+const collection = database.collection(process.env.ASTRA_DB_COLLECTION!);
+
+export async function GET() {
+  try {
+    // Ping the DB (just get one doc instead of all for efficiency)
+    const doc = await collection.findOne({});
+
+    return NextResponse.json({
+      success: true,
+      message: "Pinged Astra DB successfully",
+      doc,
+    });
+  } catch (err: any) {
+    return NextResponse.json(
+      { success: false, error: err.message },
+      { status: 500 }
+    );
+  }
+}
